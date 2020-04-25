c495585a04dcd7da88fd4eaa5558aafa5bd2ef03
paclaraujo <paclaraujo@gmail.com>
2020-04-25 10:16:12 -0300

    create unit tests

---
+++ test/cipher.spec.js
@@ -0,0 +1,61 @@
+import cipher from '../src/cipher';
+
+describe('cipher', () => {
+
+  it('should be an object', () => {
+    expect(typeof cipher).toBe('object');
+  });
+
+  describe('cipher.encode', () => {
+    it('should be a function', () => {
+      expect(typeof cipher.encode).toBe('function');
+    });
+
+    it('should throw TypeError when invoked with wrong argument types', () => {
+      expect(() => cipher.encode()).toThrow(TypeError);
+      expect(() => cipher.encode(0)).toThrow(TypeError);
+      expect(() => cipher.encode(null, [])).toThrow(TypeError);
+      expect(() => cipher.encode(0, 0)).toThrow(TypeError);
+    });
+
+    it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+      expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+
+    it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+      expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+    });
+
+    it('should return " !@" for " !@"', () => {
+      expect(cipher.encode(33, ' !@')).toEqual(' !@');
+    });
+    
+});
+
+  describe('cipher.decode', () => {
+
+    it('should be a function', () => {
+      expect(typeof cipher.decode).toBe('function');
+    });
+
+    it('should throw TypeError when invoked with wrong argument types', () => {
+      expect(() => cipher.decode()).toThrow(TypeError);
+      expect(() => cipher.decode(0)).toThrow(TypeError);
+      expect(() => cipher.decode(null, [])).toThrow(TypeError);
+      expect(() => cipher.decode(0, 0)).toThrow(TypeError);
+    });
+
+    it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
+      expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+      expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+    });
+
+    it('should return " !@" para " !@"', () => {
+      expect(cipher.decode(33, ' !@')).toEqual(' !@');
+    });
+
+  });
+});

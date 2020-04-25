c0655b2dcbf023d9f6f55e6d8fe157401406570b
paclaraujo <paclaraujo@gmail.com>
2020-04-25 16:57:00 -0300

    update unit tests

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,61 +1,42 @@
-import cipher from '../src/cipher';
+import cipher from '../src/js/cipher';
 
 describe('cipher', () => {
-
-  it('should be an object', () => {
-    expect(typeof cipher).toBe('object');
+  it('should be a function', () => {
+    expect(typeof cipher).toBe('function');
   });
 
-  describe('cipher.encode', () => {
-    it('should be a function', () => {
-      expect(typeof cipher.encode).toBe('function');
-    });
+  it('should throw TypeError when invoked with wrong argument types', () => {
+    expect(() => cipher()).toThrow(TypeError);
+    expect(() => cipher(0)).toThrow(TypeError);
+    expect(() => cipher(null, [])).toThrow(TypeError);
+    expect(() => cipher(0, 0)).toThrow(TypeError);
+  });
 
-    it('should throw TypeError when invoked with wrong argument types', () => {
-      expect(() => cipher.encode()).toThrow(TypeError);
-      expect(() => cipher.encode(0)).toThrow(TypeError);
-      expect(() => cipher.encode(null, [])).toThrow(TypeError);
-      expect(() => cipher.encode(0, 0)).toThrow(TypeError);
-    });
-
+  describe('cipher with positive direction', () => {
     it('should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
-      expect(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
+      expect(cipher(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')).toBe('HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
 
     it('should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
-      expect(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
+      expect(cipher(33, 'abcdefghijklmnopqrstuvwxyz')).toBe('hijklmnopqrstuvwxyzabcdefg');
     });
 
     it('should return " !@" for " !@"', () => {
-      expect(cipher.encode(33, ' !@')).toEqual(' !@');
+      expect(cipher(33, ' !@')).toEqual(' !@');
     });
-    
-});
+  });
 
-  describe('cipher.decode', () => {
-
-    it('should be a function', () => {
-      expect(typeof cipher.decode).toBe('function');
-    });
-
-    it('should throw TypeError when invoked with wrong argument types', () => {
-      expect(() => cipher.decode()).toThrow(TypeError);
-      expect(() => cipher.decode(0)).toThrow(TypeError);
-      expect(() => cipher.decode(null, [])).toThrow(TypeError);
-      expect(() => cipher.decode(0, 0)).toThrow(TypeError);
-    });
-
+  describe('cipher with negative direction', () => {
     it('should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
-      expect(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG')).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+      expect(cipher(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG', -1)).toBe('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
 
     it('should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
-      expect(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg')).toBe('abcdefghijklmnopqrstuvwxyz');
+      expect(cipher(33, 'hijklmnopqrstuvwxyzabcdefg', -1)).toBe('abcdefghijklmnopqrstuvwxyz');
     });
 
     it('should return " !@" para " !@"', () => {
-      expect(cipher.decode(33, ' !@')).toEqual(' !@');
+      expect(cipher(33, ' !@', -1)).toEqual(' !@');
     });
-
   });
 });

085a9e33a2411f71e8c574419e8bee14edd2e66e
paclaraujo <paclaraujo@gmail.com>
2020-04-25 16:27:31 -0300

    add data

---
+++ src/js/data.js
@@ -0,0 +1,11 @@
+export default {
+  ALPHABET_SIZE: 26,
+  UPPERCASE: {
+    MIN: "A".charCodeAt(),
+    MAX: "Z".charCodeAt()
+  },
+  LOWERCASE: {
+    MIN: "A".charCodeAt(),
+    MAX: "Z".charCodeAt()
+  }
+}
